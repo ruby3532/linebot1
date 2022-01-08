baab1f5de70be9cdc13dffd41e316ec9db7795f4
李心汝 <lixinru@lixinrudeAir.Home>
2022-01-08 16:19:50 +0800

    update fourth

--- app.py
+++ app.py
@@ -52,10 +52,10 @@ def handle_message(event):
             sticker_id='52114129'
         )
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
-    
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
 
     if msg in ['hi' , 'Hi']:
         r = '哈囉，很高興認識你～'
